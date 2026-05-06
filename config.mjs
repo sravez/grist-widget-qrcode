8b7b0c2c2e97e77098435a1d1ecf09a936457ba1
Serge RAVEZ <serge.ravez@laposte.net>
2026-05-06 16:34:01 +0200

    formulaire presque prêt

--- config.mjs
+++ config.mjs
@@ -1,27 +1,36 @@
 const form = document.getElementById("settings_form")
 
 const options = {
-	existing_size: 0,
-	computed_size: 128,
-	compute: "invalid",
-	auto_save: "invalid",
+	type: "png", 
+	display_size: 64,
+	auto_next: "invalid",
+	btn_update_all: false,
+	btn_update_invalid: true,
 	qrc_size: 256,
-	resilience: "Q",
-	foreground_color: "#FF0000",
-	background_color: "#FFFFFF",
 	margin: 12,
 	border: 30,
 	text_size: 18,
-	text_color: "#808080",
+	redundancy: "Q",
+	foreground_color: "#0000FF",
+	background_color: "#FFFFFF",
+	border_color: "#FF0000",
+	text_color: "#000000",
+	auto_test: true,
 	position: 0,
 	save_mode: "replace"
 }
 
 
-function init(a_options) {
+function init_form(a_options) {
 	for(const o in a_options) {
-		form[o].value = a_options[o]
+		if (form[o]) {
+			if(form[o].type == "checkbox") {
+				form[o].checked = a_options[o]
+			} else {
+				form[o].value = a_options[o]
+			}
+		}
 	}
 }
 
-init(options)
+init_form(options)
